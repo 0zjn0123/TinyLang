fd3cb07f81b75633c01203fd7c23161d8da03dd4
zjnautoz <zjnautoz@outlook.com>
2024-09-07 23:21:51 +0800

    v1.0.0-beta refactor(main): 实现代码解释器的初步功能
    - 添加解释器的核心框架，能够读取和解析代码列表。
    - 实现提取函数调用的函数，以解析代码中的函数调用。
    -引入代码运行函数，能够处理代码的执行，包括对函数调用的解析和执行。
    -重构条件语句，提高代码的整洁度和可读性。
    
    refactor(test): 清理测试文件，添加简单的测试案例
    
    - 创建新的测试案例以验证基础代码解释器的功能。
    - 通过注释标记已废弃的测试条目，为未来的彻底移除做准备。

--- main.py
+++ main.py
@@ -3,12 +3,11 @@
 import os
 import re
 import sys
-print(sys.argv)
+
 try:
     if not sys.argv[1]:
         print("TinyChatMode")
-    elif os.path.isfile(sys.argv[1]):
-        print(sys.argv[1][-3:])
+
         
     try:
         target_file = open(sys.argv[1], "r")
@@ -22,17 +21,52 @@ except:
 
 
 code_list = re.split("\n|;",target_file.read())
-print(code_list)
+
 
 
 
 
 # 解释部分
+
+
+def extract_function_calls(code):
+    # 正则表达式模式，用于匹配函数调用
+    function_call_pattern = r"(\w+)\s*\(([^)]*)\)"
+
+    # 查找所有匹配的函数调用
+    matches = re.findall(function_call_pattern, code)
+
+    # 解析并提取函数名称和参数
+    function_calls = []
+    for match in matches:
+        function_name = match[0]
+        params = match[1].strip()
+
+        # 如果参数为空，则设置为空字符串
+        if not params:
+            params = ''
+
+        function_calls.append((function_name, params))
+
+    return function_calls
+
+
 kwlist = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 funlist = {'print':"printfun"}
 def printfun(text):
     print(text)
+def runcode(code):
+    code_lstrip = code.strip()
+    if code_lstrip.startswith("#") or code_lstrip == "":
+        return None
+    # 提取函数调用
+    function_calls = extract_function_calls(code_lstrip)
+    if function_calls:
+        if funlist.get(function_calls[0][0], False):
+
+            exec(funlist[function_calls[0][0]]+"("+function_calls[0][1]+")")
+
+
 for code in code_list:
-    if not code[0:] == '#':
-        if code in funlist or code in kwlist:
-            exec(funlist[code])
+    print(code)
+    runcode(code)
